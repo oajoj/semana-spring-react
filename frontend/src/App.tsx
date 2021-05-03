5b875d3549f3620c3fb8b9f05c29f9811c1dd271
João Paulo Costa <joao-paulocosta@live.com>
2021-05-03 17:14:17 -0300

    Componentes básicos estáticos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,20 @@
 //import React from 'react';
 
+import DataTable from "components/dataTable";
+import Footer from "components/footer";
+import NavBar from "components/navbar";
+
 function App() {
   return (
     <div>
-      <h1 className="text-primary">Olá mundo</h1>
+      <>
+        <NavBar />
+        <div className="container">
+          <h1 className="text-primary">Olá mundo</h1>
+          <DataTable />
+        </div>
+        <Footer />
+      </>
     </div>
   );
 }
---
+++ frontend/src/components/dataTable/index.tsx
@@ -0,0 +1,98 @@
+const DataTable = () => {
+    return (
+        <div className="table-responsive">
+            <table className="table table-striped table-sm">
+                <thead>
+                    <tr>
+                        <th>Data</th>
+                        <th>Vendedor</th>
+                        <th>Clientes Visitados</th>
+                        <th>Negócios fechados</th>
+                        <th>Valor</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th>22/04/2021</th>
+                        <th>Silvio Santos</th>
+                        <th>1354</th>
+                        <th>1200</th>
+                        <th>257000.00</th>
+                    </tr>
+                    <tr>
+                        <th>22/04/2021</th>
+                        <th>Silvio Santos</th>
+                        <th>1354</th>
+                        <th>1200</th>
+                        <th>257000.00</th>
+                    </tr>
+                    <tr>
+                        <th>22/04/2021</th>
+                        <th>Silvio Santos</th>
+                        <th>1354</th>
+                        <th>1200</th>
+                        <th>257000.00</th>
+                    </tr>
+                    <tr>
+                        <th>22/04/2021</th>
+                        <th>Silvio Santos</th>
+                        <th>1354</th>
+                        <th>1200</th>
+                        <th>257000.00</th>
+                    </tr>
+                    <tr>
+                        <th>22/04/2021</th>
+                        <th>Silvio Santos</th>
+                        <th>1354</th>
+                        <th>1200</th>
+                        <th>257000.00</th>
+                    </tr>
+                    <tr>
+                        <th>22/04/2021</th>
+                        <th>Silvio Santos</th>
+                        <th>1354</th>
+                        <th>1200</th>
+                        <th>257000.00</th>
+                    </tr>
+                    <tr>
+                        <th>22/04/2021</th>
+                        <th>Silvio Santos</th>
+                        <th>1354</th>
+                        <th>1200</th>
+                        <th>257000.00</th>
+                    </tr>
+                    <tr>
+                        <th>22/04/2021</th>
+                        <th>Silvio Santos</th>
+                        <th>1354</th>
+                        <th>1200</th>
+                        <th>257000.00</th>
+                    </tr>
+                    <tr>
+                        <th>22/04/2021</th>
+                        <th>Silvio Santos</th>
+                        <th>1354</th>
+                        <th>1200</th>
+                        <th>257000.00</th>
+                    </tr>
+                    <tr>
+                        <th>22/04/2021</th>
+                        <th>Silvio Santos</th>
+                        <th>1354</th>
+                        <th>1200</th>
+                        <th>257000.00</th>
+                    </tr>
+                    <tr>
+                        <th>22/04/2021</th>
+                        <th>Silvio Santos</th>
+                        <th>1354</th>
+                        <th>1200</th>
+                        <th>257000.00</th>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default DataTable;
